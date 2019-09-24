9d48ac2eafff34d0d6692c700a0e49f718d4da4a
Eugene Han <hanjie1030@gmail.com>
2019-09-24 20:33:30 +0800

    data preprocessing

--- q1.py
+++ q1.py
@@ -1,7 +1,19 @@
 import scipy.io as sio
+import numpy as np
+from sklearn import preprocessing
+
 mat = sio.loadmat('spamData.mat')
 
 xtest = mat['Xtest']
 xtrain = mat['Xtrain']
 ytest = mat['ytest']
 ytrain = mat['ytrain']
+
+# Binarization preprocessing
+binarizer = preprocessing.Binarizer().fit(xtest)
+xtest = binarizer.transform(xtest)
+print(xtest)
+
+binarizer = preprocessing.Binarizer().fit(xtrain)
+xtrain = binarizer.transform(xtrain)
+print(xtrain)
