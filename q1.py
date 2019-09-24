233fddb9317c4d15312ffe06f0d10fbef091f15b
Eugene Han <hanjie1030@gmail.com>
2019-09-24 22:00:46 +0800

    add steleton

--- q1.py
+++ q1.py
@@ -2,18 +2,48 @@ import scipy.io as sio
 import numpy as np
 from sklearn import preprocessing
 
-mat = sio.loadmat('spamData.mat')
 
-xtest = mat['Xtest']
-xtrain = mat['Xtrain']
-ytest = mat['ytest']
-ytrain = mat['ytrain']
+def loadData(fileName):
+	mat = sio.loadmat(fileName)
+	xtest = mat['Xtest']
+	xtrain = mat['Xtrain']
+	ytest = mat['ytest']
+	ytrain = mat['ytrain']
+	return xtrain, ytrain, xtest, ytest
 
 # Binarization preprocessing
-binarizer = preprocessing.Binarizer().fit(xtest)
-xtest = binarizer.transform(xtest)
-print(xtest)
+def binarization(a):
+	binarizer = preprocessing.Binarizer().fit(a)
+	return binarizer.transform(a)
 
-binarizer = preprocessing.Binarizer().fit(xtrain)
-xtrain = binarizer.transform(xtrain)
-print(xtrain)
+def getLambdaML(data):
+	a = np.array(data)
+	unique, counts = np.unique(a, return_counts=True)
+	labels = dict(zip(unique, counts))
+	# ML Estimate of lambda = N1/N
+	return labels[1]/(labels[0]+labels[1])
+
+def getNbClassifier(xtrain, ytrain):
+	# lambdaMl = getLambdaML(ytrain)
+	# Separate xtrain by class/label
+	spam = []
+	nonspam = []
+	for i in range(len(xtrain)):
+		vector = xtrain[i]
+		if ytrain[i] == 0:
+			nonspam.append(vector)
+		else:
+			spam.append(vector)
+
+def main():
+	# Load data from spamData.mat
+	xtrain, ytrain, xtest, ytest = loadData('./spamData.mat')
+
+	# Get classifier
+	naiveBayesClassifier = getNbClassifier(xtrain, ytrain)
+	# prepare model
+	summaries = summarizeByClass(trainingSet)
+	# test model
+	predictions = getPredictions(summaries, testSet)
+	accuracy = getAccuracy(testSet, predictions)
+	print('Accuracy: {0}%').format(accuracy)
