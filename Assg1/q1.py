ad103ce6a82a80daad571764f7e5a136a7ae2285
Eugene Han <hanjie1030@gmail.com>
2019-09-26 22:36:24 +0800

    remove unnecessary commands

--- Assg1/q1.py
+++ Assg1/q1.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
+# To add a new cell, type '#%%'
+# To add a new markdown cell, type '#%% [markdown]'
 
 #%%
 import scipy.io as sio
@@ -10,29 +10,29 @@ import math
 import matplotlib.pyplot as plot
 
 def getNbClassifier(xtrain, ytrain):
-	# Separate xtrain by class/label
-	spam = []
-	nonspam = []
-	for i in range(len(xtrain)):
-		vector = xtrain[i]
-		if ytrain[i] == 0:
-			nonspam.append(vector)
-		else:
-			spam.append(vector)
-	spam = np.array(spam).astype(int)
-	nonspam = np.array(nonspam).astype(int)
-	# calculate Xcj matrix
-	numFeatures = spam.shape[1]
-	print('Feauture Number: ', numFeatures)
-	x = np.zeros((2, numFeatures)).astype(int)
-	x[1] = np.sum(spam, axis=0)
-	x[0] = np.sum(nonspam, axis=0)
-	print(x[0])
-	print(x[1])
-	return x
+    # Separate xtrain by class/label
+    spam = []
+    nonspam = []
+    for i in range(len(xtrain)):
+        vector = xtrain[i]
+        if ytrain[i] == 0:
+            nonspam.append(vector)
+        else:
+            spam.append(vector)
+    spam = np.array(spam).astype(int)
+    nonspam = np.array(nonspam).astype(int)
+    # calculate Xcj matrix
+    numFeatures = spam.shape[1]
+    print('Feauture Number: ', numFeatures)
+    x = np.zeros((2, numFeatures)).astype(int)
+    x[1] = np.sum(spam, axis=0)
+    x[0] = np.sum(nonspam, axis=0)
+    print(x[0])
+    print(x[1])
+    return x
 
 def getPredictions(classifier, xtest):
-	return
+    return
 
 
 #%%
@@ -68,66 +68,66 @@ errRate = np.zeros(alphaArr.shape[0])
 #%%
 
 def calcPosteriorProdictiveDist(alpha, naiveBayesClassifier, lambdaMl, featureVector):
-	# Initialise
-	lambdaMlArr = np.array([1 - lambdaMl, lambdaMl])
-	logP_yTilde = np.log(lambdaMlArr)
-	#print('lambdaMlArr ',logP_yTilde)
+    # Initialise
+    lambdaMlArr = np.array([1 - lambdaMl, lambdaMl])
+    logP_yTilde = np.log(lambdaMlArr)
+    #print('lambdaMlArr ',logP_yTilde)
     
-	#print('featureVector ', featureVector)
-	#print('p(y=0):')
+    #print('featureVector ', featureVector)
+    #print('p(y=0):')
 
-	# Calculate  logP(yTilde = 0 | xTilde, D)
-	for i in range(featureVector.shape[0]):
-		feature = featureVector[i]
-		if (feature):
-			n1 = naiveBayesClassifier[0][i]
-		else:
-			n1 = N1 - naiveBayesClassifier[0][i]
-		prior = (n1 + alpha)/(N1 + 2* alpha)
-		#print('n1: ', n1, 'N: ', N1)
-		#print('prior: ', prior)
-		if prior == 0:
-			prior = 0.00000001
-		#print('logP_yTilde', logP_yTilde[0])
-		logP_yTilde[0] += math.log(prior)
+    # Calculate  logP(yTilde = 0 | xTilde, D)
+    for i in range(featureVector.shape[0]):
+        feature = featureVector[i]
+        if (feature):
+            n1 = naiveBayesClassifier[0][i]
+        else:
+            n1 = N1 - naiveBayesClassifier[0][i]
+        prior = (n1 + alpha)/(N1 + 2* alpha)
+        #print('n1: ', n1, 'N: ', N1)
+        #print('prior: ', prior)
+        if prior == 0:
+            prior = 0.00000001
+        #print('logP_yTilde', logP_yTilde[0])
+        logP_yTilde[0] += math.log(prior)
         
-	#print('p(y=1):')
-	# Calculate  logP(yTilde = 1 | xTilde, D)
-	for i in range(featureVector.shape[0]):
-		feature = featureVector[i]
-		if (feature):
-			n1 = naiveBayesClassifier[1][i]
-		else:
-			n1 = N - N1 - naiveBayesClassifier[1][i]
-		#print('n1: ', n1, 'N: ', N - N1)
-		prior = (n1 + alpha)/((N - N1) + 2* alpha)
-		#print('prior: ', prior)
-		if prior == 0:
-			prior = 0.1
-		logP_yTilde[1] += math.log(prior)
-	#print('logP_yTilde_0',logP_yTilde[0])
-	#print('logP_yTilde_1',logP_yTilde[1])
-	if logP_yTilde[0] > logP_yTilde[1]:
-		return 0
-	else:
-		return 1
+    #print('p(y=1):')
+    # Calculate  logP(yTilde = 1 | xTilde, D)
+    for i in range(featureVector.shape[0]):
+        feature = featureVector[i]
+        if (feature):
+            n1 = naiveBayesClassifier[1][i]
+        else:
+            n1 = N - N1 - naiveBayesClassifier[1][i]
+        #print('n1: ', n1, 'N: ', N - N1)
+        prior = (n1 + alpha)/((N - N1) + 2* alpha)
+        #print('prior: ', prior)
+        if prior == 0:
+            prior = 0.1
+        logP_yTilde[1] += math.log(prior)
+    #print('logP_yTilde_0',logP_yTilde[0])
+    #print('logP_yTilde_1',logP_yTilde[1])
+    if logP_yTilde[0] > logP_yTilde[1]:
+        return 0
+    else:
+        return 1
     
 
 
 #%%
 
 for j in range(alphaArr.shape[0]):
-	alpha = alphaArr[j]
-	print('alpha: ', alpha)
-	predictedRes = np.zeros(xtest.shape[0])
-	for i in range(xtest.shape[0]):
-		featureVector = xtest[i]
-		predictedRes[i] = calcPosteriorProdictiveDist(alpha, naiveBayesClassifier, lambdaMl, featureVector)
-		#print('predicted: ', predictedRes[i])
-	predictedRes = predictedRes.astype(int)
-	ytest = ytest.flatten().astype(int)
-	errRate[j] = np.mean( predictedRes != ytest )
-	print(errRate[j])
+    alpha = alphaArr[j]
+    print('alpha: ', alpha)
+    predictedRes = np.zeros(xtest.shape[0])
+    for i in range(xtest.shape[0]):
+        featureVector = xtest[i]
+        predictedRes[i] = calcPosteriorProdictiveDist(alpha, naiveBayesClassifier, lambdaMl, featureVector)
+        #print('predicted: ', predictedRes[i])
+    predictedRes = predictedRes.astype(int)
+    ytest = ytest.flatten().astype(int)
+    errRate[j] = np.mean( predictedRes != ytest )
+    print(errRate[j])
 
 #%%
 
--- Assg1/q2.py
+++ Assg1/q2.py
@@ -1,13 +1,5 @@
 # To add a new cell, type '#%%'
 # To add a new markdown cell, type '#%% [markdown]'
-#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
-# ms-python.python added
-import os
-try:
-    os.chdir(os.path.join(os.getcwd(), 'EE5907Assignments/Assg1'))
-    print(os.getcwd())
-except:
-    pass
 
 #%%
 import scipy.io as sio
