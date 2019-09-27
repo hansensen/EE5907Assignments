9b01eb2c72382b3a31666996b3acdcf87d70f279
Eugene Han <hanjie1030@gmail.com>
2019-09-27 21:00:23 +0800

    get lambda list

--- Assg1/q3.py
+++ Assg1/q3.py
@@ -1,3 +1,4 @@
+# %%
 import scipy.io as sio
 import numpy as np
 import DataUtil as du
@@ -6,6 +7,15 @@ import matplotlib.pyplot as plot
 import scipy.stats as sst
 
 np.seterr(divide='ignore')
+
+
+# %%
+# get an array of lambda
+def getLambda():
+    lambda1 = np.arange(1, 11, 1)
+    lambda2 = np.arange(15, 105, 5)
+    return np.concatenate((lambda1, lambda2), axis=0)
+
 
 # %%
 # Load data from spamData.mat
